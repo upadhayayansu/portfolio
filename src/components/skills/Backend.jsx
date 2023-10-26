fd46c6d2b936c7e2e0741ad1379761aedc14faac
upadhayayansu <upadhayayansu8@gmail.com>
2023-10-26 22:58:07 +0545

    skills added

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Backend developer</h3>
+
+      <div className="skills__box">
+        <div className="skills__group"></div>
+        <div className="skills_-data"></div>
+        <i className="bx bx-badge-check"></i>
+        <div>
+          <h3 className="skills__name">php</h3>
+          <span className="skills__level">Basic</span>
+        </div>
+      </div>
+      <div className="skills_-data"></div>
+      <i className="bx bx-badge-check"></i>
+      <div>
+        <h3 className="skills__name">Node js</h3>
+        <span className="skills__level">Basic</span>
+      </div>
+      <div className="skills_-data"></div>
+      <i className="bx bx-badge-check"></i>
+      <div>
+        <h3 className="skills__name">MySQL</h3>
+        <span className="skills__level">Intermediate</span>
+      </div>
+    </div>
+  );
+};
+
+export default Backend;
