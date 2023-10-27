c48899d28b3a3b369074f5ceb273a6652468f650
upadhayayansu <upadhayayansu8@gmail.com>
2023-10-27 22:49:44 +0545

    adds

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Qualification.css";
 
 const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggletab = (index) => {
+    setToggleState(index);
+  };
   return (
     <section className="qualification section">
       <h2 className="section__title">Qualification</h2>
       <span className="section__subtitle">MY personal JOurney</span>
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div className="qualification__button qualification__active button--flex">
+          <div className="{toggleState=== 1qualification__button qualification__active button--flex">
             <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
